e279ee6c7884099b53560310a9a3ec51afb98b2a
Сергей Прокопенко <psi200523@gmail.com>
2025-06-09 02:37:53 +1000

    карточка

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,17 +1,35 @@
 "use client";
+import Silk from '@/components/Silk';
+import ProfileCard from '@/components/ProfileCard';
 
-import Silk from '@/components/BackgroundWebP';
 export default function Home() {
   return (
-    <div className=" w-screen h-screen fixed top-0 left-0 z-[-1]">
-      <Silk
-        speed={3}
-        scale={1}
-        color="#7B588D"
-        noiseIntensity={2.2}
-        rotation={6.23}
-      />
-    </div>
+    <div className="relative w-screen h-screen overflow-hidden">
+      {/* Фон Silk */}
+      <div className="absolute inset-0 -z-10">
+        <Silk
+          speed={3}
+          scale={1}
+          color="#7B588D"
+          noiseIntensity={2.2}
+          rotation={6.23}
+        />
+      </div>
 
+      {/* Центрированный контент */}
+      <div className="flex items-center justify-center w-1/2 h-full">
+        <ProfileCard
+          name="Прокопенко Сергей Игоревич"
+          title="Developer"
+          handle="ProkopenkoSR"
+          status="Online"
+          contactText="Напиши мне"
+          avatarUrl="png/avatar.png"
+          showUserInfo={true}
+          enableTilt={true}
+          onContactClick={() => console.log('Contact clicked')}
+        />
+      </div>
+    </div>
   );
 }
