0f0c96d912eacf9718e66570fdddd9c537a0a9b4
sergay <psi200523@gmail.com>
2025-08-24 17:32:29 +0900

    whoim

--- src/components/terminals/whoami.tsx
+++ src/components/terminals/whoami.tsx
@@ -4,6 +4,8 @@ import Image from "next/image"
 import { useState } from "react"
 import TerminalWindow from "../TerminalWindow"
 import Typewriter from "../Typewriter"
+import FaultyTerminal from '../FaultyTerminal';
+
 
 interface TerminalProps {
   onClose?: () => void
@@ -24,17 +26,27 @@ export default function WhoamiTerminal({ onClose, onMinimize, onToggleMaximize }
       <div className="flex flex-col lg:flex-row items-center lg:items-start gap-6">
 
         <div className="w-full max-w-xl min-h-[170px] bg-black relative rounded-xl overflow-hidden">
-          <Image
-            src="/avatar.webp"
-            alt="Avatar"
-            fill
-            className={`object-cover object-[25%_35%] select-none pointer-events-none transition-opacity duration-700 ease-in-out ${
-              loaded ? "opacity-100" : "opacity-0"
-            }`}
-            draggable={false}
-            onLoad={() => setLoaded(true)}
-            priority
-          />
+          <div className="w-full h-[160px] relative">
+            <FaultyTerminal
+              scale={1.0}
+              gridMul={[3, 1]}
+              digitSize={1.2}
+              timeScale={1}
+              pause={false}
+              scanlineIntensity={1}
+              glitchAmount={1}
+              flickerAmount={1}
+              noiseAmp={1}
+              chromaticAberration={0}
+              dither={0}
+              curvature={0}
+              tint="#a7ef9e"
+              mouseReact={true}
+              mouseStrength={0.5}
+              pageLoadAnimation={true}
+              brightness={1}
+            />
+          </div>
         </div>
 
         <div className="space-y-2 text-left">
